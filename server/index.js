d1936a622034bf4ad5b5f4834f4492bf23efba68
Sorawong L <Sorawong2543@gmail.com>
2021-05-11 14:29:56 +0700

    Update login system

--- server/index.js
+++ server/index.js
@@ -1,58 +1,86 @@
-const express = require("express");
-const User = require("./model").User;
-const Game = require("./model").Game;
-const DLC = require("./model").DLC;
-const Review = require("./model").Review;
-const Transaction = require("./model").Transaction;
-const Promotion = require("./model").Promotion;
-const Group = require("./model").Group;
-const bodyParser = require("body-parser");
-const app = express();
+const express = require("express")
+const bodyParser = require("body-parser")
+const session = require("express-session")
+const User = require("./model").User
+const Game = require("./model").Game
+const DLC = require("./model").DLC
+const Review = require("./model").Review
+const Transaction = require("./model").Transaction
+const Promotion = require("./model").Promotion
+const Group = require("./model").Group
+const app = express()
 
-app.use(express.static("../client/public")); //Set static floder (.css)
-app.set("views", "../client/public"); //Set views folder (.ejs)
-app.set("view engine", "ejs"); //Set view engine
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("../client/public")) //Set static floder (.css)
+app.set("views", "../client/public") //Set views folder (.ejs)
+app.set("view engine", "ejs") //Set view engine
+app.use(bodyParser.urlencoded({ extended: true })) //Set bodyParser
+app.use(
+  session({
+    secret: "login",
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 
 app.get("/", (request, response) => {
-  response.render("index");
-});
+  var sessionUsername = request.session.username
+  response.render("index", { username: sessionUsername })
+})
 
 app.get("/store", (request, response) => {
-  response.render("store");
-});
+  response.render("store")
+})
 
 app.get("/about", (request, response) => {
-  response.render("about");
-});
+  response.render("about")
+})
 
 app.get("/signup", (request, respone) => {
-  respone.render("signup");
-});
+  respone.render("signup")
+})
 
 app.get("/staffs", (request, respone) => {
-  respone.render("staffs");
-});
+  respone.render("staffs")
+})
 
 app.all("/login", (request, response) => {
-  var username = request.body.username;
-  var password = request.body.password;
+  var username = request.body.username
+  var password = request.body.password
   if (username && password) {
     User.find({
       $and: [{ username: { $eq: username } }, { password: { $eq: password } }],
     }).exec((err, doc) => {
       if (doc.length > 0) {
-        var name = doc[0].fName;
-        response.send(`Hello ${name}`);
+        //username & password are in database
+        request.session.username = doc[0].username
+        response.redirect("/")
       } else {
-        response.send(`Incorrect username or password`);
+        response.render("login", { logined: true, success: false })
       }
-    });
+    })
   } else {
-    response.render("login");
+    //username & password not in database
+    response.render("login", { logined: false, success: false })
   }
-});
+})
+
+app.get("/logout", (request, response) => {
+  request.session.destroy((error) => {
+    if (!error) {
+      response.redirect("/")
+    } else {
+      console.log(error)
+    }
+  })
+})
+
+app.get("/userinfo", (request, response) => {
+  var sessionUsername = request.session.username
+  User.find({ username: { $eq: sessionUsername } }).exec((err, doc) => {
+    response.render("userinfo", doc[0])
+  })
+})
 
 app.listen(3000, () => {
-  console.log("Server started at : http://localhost:3000");
-});
+  console.log("Server started at : http://localhost:3000")
+})
