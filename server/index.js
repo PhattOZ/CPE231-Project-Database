0c7f8a0eb2a9ae5f80f11afde96b4557f0b875b5
PhattOZ <phatto2543@gmail.com>
2021-05-12 14:26:27 +0700

    add to about.ejs

--- server/index.js
+++ server/index.js
@@ -1,86 +1,86 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const session = require("express-session")
-const User = require("./model").User
-const Game = require("./model").Game
-const DLC = require("./model").DLC
-const Review = require("./model").Review
-const Transaction = require("./model").Transaction
-const Promotion = require("./model").Promotion
-const Group = require("./model").Group
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const User = require("./model").User;
+const Game = require("./model").Game;
+const DLC = require("./model").DLC;
+const Review = require("./model").Review;
+const Transaction = require("./model").Transaction;
+const Promotion = require("./model").Promotion;
+const Group = require("./model").Group;
+const app = express();
 
-app.use(express.static("../client/public")) //Set static floder (.css)
-app.set("views", "../client/public") //Set views folder (.ejs)
-app.set("view engine", "ejs") //Set view engine
-app.use(bodyParser.urlencoded({ extended: true })) //Set bodyParser
+app.use(express.static("../client/public")); //Set static floder (.css)
+app.set("views", "../client/public"); //Set views folder (.ejs)
+app.set("view engine", "ejs"); //Set view engine
+app.use(bodyParser.urlencoded({ extended: true })); //Set bodyParser
 app.use(
   session({
     secret: "login",
     resave: false,
     saveUninitialized: false,
   })
-)
+);
 
 app.get("/", (request, response) => {
-  var sessionUsername = request.session.username
-  response.render("index", { username: sessionUsername })
-})
+  var sessionUsername = request.session.username;
+  response.render("index", { username: sessionUsername });
+});
 
 app.get("/store", (request, response) => {
-  response.render("store")
-})
+  response.render("store");
+});
 
 app.get("/about", (request, response) => {
-  response.render("about")
-})
+  response.render("about");
+});
 
 app.get("/signup", (request, respone) => {
-  respone.render("signup")
-})
+  respone.render("signup");
+});
 
-app.get("/staffs", (request, respone) => {
-  respone.render("staffs")
-})
+app.get("/aboutus", (request, respone) => {
+  respone.render("about");
+});
 
 app.all("/login", (request, response) => {
-  var username = request.body.username
-  var password = request.body.password
+  var username = request.body.username;
+  var password = request.body.password;
   if (username && password) {
     User.find({
       $and: [{ username: { $eq: username } }, { password: { $eq: password } }],
     }).exec((err, doc) => {
       if (doc.length > 0) {
         //username & password are in database
-        request.session.username = doc[0].username
-        response.redirect("/")
+        request.session.username = doc[0].username;
+        response.redirect("/");
       } else {
-        response.render("login", { logined: true, success: false })
+        response.render("login", { logined: true, success: false });
       }
-    })
+    });
   } else {
     //username & password not in database
-    response.render("login", { logined: false, success: false })
+    response.render("login", { logined: false, success: false });
   }
-})
+});
 
 app.get("/logout", (request, response) => {
   request.session.destroy((error) => {
     if (!error) {
-      response.redirect("/")
+      response.redirect("/");
     } else {
-      console.log(error)
+      console.log(error);
     }
-  })
-})
+  });
+});
 
 app.get("/userinfo", (request, response) => {
-  var sessionUsername = request.session.username
+  var sessionUsername = request.session.username;
   User.find({ username: { $eq: sessionUsername } }).exec((err, doc) => {
-    response.render("userinfo", doc[0])
-  })
-})
+    response.render("userinfo", doc[0]);
+  });
+});
 
 app.listen(3000, () => {
-  console.log("Server started at : http://localhost:3000")
-})
+  console.log("Server started at : http://localhost:3000");
+});
