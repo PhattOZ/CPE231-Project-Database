dbd64a08ef947f8e0ded5c7de8939645104a0c98
PhattOZ <phatto2543@gmail.com>
2021-05-14 21:05:49 +0700

    update index.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { kanit: ["Kanit"] },
+      colors: {
+        cyan: {
+          lightest: "#A9CCE3",
+          lighter: "#7FB3D5",
+          light: "#5499C7",
+          normal: "#2980B9",
+          dark: "#2471A3",
+          darker: "#1F618D",
+          darkest: "#1A5276",
+          black: "#072234",
+        },
+      },
     },
   },
   variants: {
