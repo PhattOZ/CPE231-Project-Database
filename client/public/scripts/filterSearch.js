472733f69d6dc2f0360d13649d0b8b69986f5cb0
PhattOZ <phatto2543@gmail.com>
2021-05-31 18:03:44 +0700

    Add category bar to addGame

---
+++ client/public/scripts/filterSearch.js
@@ -0,0 +1,8 @@
+function filterSearch() {
+  filter = input.value.toUpperCase();
+  availableTags = ["RPG", "Adventure"];
+
+  for (let i = 0; i < availableTags.length; i++) {}
+}
+
+function enableInput() {}
